2ba1c46023dcaedbf9ce5677f8da8f538cabfbc8
Анастасия Федотова <pikmi_computer@MacBook-Air-Anastasia.local>
2026-03-09 01:12:25 +0300

    Fix config import

---
+++ config.js
@@ -0,0 +1,4 @@
+export const SUPABASE_CONFIG = {
+  url: 'https://miovofajmsssdhkkpbme.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pb3ZvZmFqbXNzc2Roa2twYm1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MDgwNzMsImV4cCI6MjA4ODM4NDA3M30.fjehq2rTP7UXcwruK0npJRypD7yxbNA-KVPTo9MI6T0'
+};
